5f0747596f4225f3333ac95e99210fcdf2498ba0
PASINDU999 <mpasindu72@gmail.com>
2022-10-27 22:36:53 +0530

    Adding basic functionalities

--- Construction/src/main/java/com/model/products.java
+++ Construction/src/main/java/com/model/products.java
@@ -1,14 +1,73 @@
 package com.model;
 
+import java.sql.Date;
+
 public class products {
-	
-	int productName;
+	int id;
+	String productName;
 	String Description;
 	String Supplier;
 	int mobile;
 	int quantity;
 	double price;
 	double SubTotal;
+	Date date;
 	
+	public int getId() {
+		return id;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public String getSupplier() {
+		return Supplier;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setSupplier(String supplier) {
+		Supplier = supplier;
+	}
+	public int getMobile() {
+		return mobile;
+	}
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getSubTotal() {
+		return SubTotal;
+	}
+	public void setSubTotal(double subTotal) {
+		SubTotal = subTotal;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setDate(Date date) {
+		this.date=date;
+	}
 
 }
